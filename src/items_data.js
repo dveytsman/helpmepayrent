402098b9b1dc09ee52f22a8d9aedce407dd551b3
David Veytsman <david.veytsman@gmail.com>
2024-06-12 23:26:40 -0400

    modified webpack, removed images and hosted them on imgur for now

--- src/items_data.js
+++ src/items_data.js
@@ -1,13 +1,3 @@
-// // import sailor_northern_lights from "./images/sailor_northern_lights.jpg";
-// import sailor_cranberry from "./images/sailor_cranberry.jpg";
-// import sailor_cranberry_1 from "./images/sailor_cranberry_1.jpg";
-// import sailor_cranberry_2 from "./images/sailor_cranberry_2.jpg";
-// import apple_magic_keyboard_1 from "./images/apple_magic_keyboard_1.jpg";
-// import apple_magic_keyboard_2 from "./images/apple_magic_keyboard_2.jpg";
-// import keychron_k3_1 from "./images/keychron_k3_1.jpg";
-// import keychron_k3_2 from "./images/keychron_k3_2.jpg";
-// import nuphy_air_65_v2_1 from "./images/nuphy_air_65_v2_1.jpg";
-
 const items = [
   {
     id: 1,
@@ -15,7 +5,7 @@ const items = [
     type: "Fountain Pen",
     itemName: "Sailor Pro Gear Northern Lights Purple",
     description: "21k Broad nib. Afaik not available from goulet pens anymore.",
-    images: ["https://imgur.com/ttXekAZ"],
+    images: ["https://i.imgur.com/ttXekAZ.jpeg"],
   },
   {
     id: 2,
@@ -24,9 +14,9 @@ const items = [
     itemName: "Sailor 1911L Sparkling Cranberry",
     description: "21k Music nib",
     images: [
-      "https://imgur.com/9prXN7Z",
-      "https://imgur.com/QioFxLx",
-      "https://imgur.com/dwSbr5j",
+      "https://i.imgur.com/dwSbr5j.jpeg",
+      "https://i.imgur.com/QioFxLx.jpeg",
+      "https://i.imgur.com/9prXN7Z.jpeg",
     ],
   },
   {
@@ -35,7 +25,10 @@ const items = [
     type: "Keyboard",
     itemName: "Apple Magic Keyboard",
     description: "in perfect working order, some minor signs of use",
-    images: ["https://imgur.com/9IpzQJd", "https://imgur.com/9mcO90n"],
+    images: [
+      "https://i.imgur.com/9mcO90n.jpeg",
+      "https://i.imgur.com/9IpzQJd.jpeg",
+    ],
   },
   {
     id: 4,
@@ -44,7 +37,10 @@ const items = [
     itemName: "Keychron K3 Pro",
     description:
       "This is the non hot swappable version. White backlight, brown low profile switches. Perfect condition and it comes with the box",
-    images: ["https://imgur.com/tLBQMpD", "https://imgur.com/3mjj55y"],
+    images: [
+      "https://i.imgur.com/3mjj55y.jpeg",
+      "https://i.imgur.com/tLBQMpD.jpeg",
+    ],
   },
   {
     id: 5,
@@ -53,7 +49,7 @@ const items = [
     itemName: "Nuphy Air 65V2",
     description:
       "This is in like new condition. I can put any of the following switches in the keyboard: low profile gateron blue switches, low profile gateron brown switches, cowberry switches, moss switches, or wisteria switches",
-    images: ["https://imgur.com/IjF4QAW"],
+    images: ["https://i.imgur.com/IjF4QAW.jpg.com/IjF4QAW"],
   },
 ];
 
