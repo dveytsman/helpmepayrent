4a208430cbd307f37eee6732041f5a5f04501461
David Veytsman <david.veytsman@gmail.com>
2024-07-01 16:52:30 -0400

    updating deploy process

--- webpack.prod.js
+++ webpack.prod.js
@@ -76,50 +76,50 @@ module.exports = {
     open: true,
   },
 };
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+// const path = require("path");
+// const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-  entry: "./src/index.js",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "public"),
-    publicPath: "/",
-  },
-  mode: "production",
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
-          },
-        },
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".js", ".jsx"],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-      filename: "index.html",
-    }),
-  ],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "public"),
-    },
-    compress: true,
-    port: 9000,
-    open: true,
-  },
-};
+// module.exports = {
+//   entry: "./src/index.js",
+//   output: {
+//     filename: "bundle.js",
+//     path: path.resolve(__dirname, "public"),
+//     publicPath: "/",
+//   },
+//   mode: "production",
+//   module: {
+//     rules: [
+//       {
+//         test: /\.(js|jsx)$/,
+//         exclude: /node_modules/,
+//         use: {
+//           loader: "babel-loader",
+//           options: {
+//             presets: ["@babel/preset-env", "@babel/preset-react"],
+//           },
+//         },
+//       },
+//       {
+//         test: /\.css$/,
+//         use: ["style-loader", "css-loader"],
+//       },
+//     ],
+//   },
+//   resolve: {
+//     extensions: [".js", ".jsx"],
+//   },
+//   plugins: [
+//     new HtmlWebpackPlugin({
+//       template: "./public/index.html",
+//       filename: "index.html",
+//     }),
+//   ],
+//   devServer: {
+//     static: {
+//       directory: path.join(__dirname, "public"),
+//     },
+//     compress: true,
+//     port: 9000,
+//     open: true,
+//   },
+// };
