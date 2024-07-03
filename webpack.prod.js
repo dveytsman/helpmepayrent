b29a28fc1186d695655d9a5625a811bb3e8acd5e
David Veytsman <david.veytsman@gmail.com>
2024-07-03 15:37:09 -0400

    fixed the prod webpack config for deployment

--- webpack.prod.js
+++ webpack.prod.js
@@ -84,7 +84,7 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "public"),
+    path: path.resolve(__dirname, "build"),
     publicPath: "/",
   },
   mode: "production",
